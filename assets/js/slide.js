95edc35eea9208107ba8f1c3608cb8ccf776ec43
anexotecnologia <anderson@anexotecnologia.com.br>
2024-11-22 09:31:10 -0300

    slide

---
+++ assets/js/slide.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach(slide => slide.style.display = 'none');
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 5000); // Troca a cada 5 segundos
+}
+
+function changeSlide(n) {
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach(slide => slide.style.display = 'none');
+  slideIndex += n;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  if (slideIndex < 1) { slideIndex = slides.length; }
+  slides[slideIndex - 1].style.display = 'block';
+}
